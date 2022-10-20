55d6d39826c95f9eccc443a23e9dbc824cfd564a
Helen_Kerenykevych <helenchekanovska@gmail.com>
2022-10-21 00:28:24 +0300

    create second project

---
+++ src/main/java/com/course/second_project_gr2/SecondProjectGr2Application.java
@@ -0,0 +1,13 @@
+package com.course.second_project_gr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProjectGr2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondProjectGr2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/second_project_gr2/SecondProjectGr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.second_project_gr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondProjectGr2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
